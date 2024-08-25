5cd6281f1e24dea29d3b748774629fb82e5d90f0
Vaibhav Jadhav <vaibhavvijayjadhav@gmail.com>
2024-08-25 20:55:32 +0530

    All the file upto 22-08-2024 are added

--- src/ex_22082024/Lab040.py
+++ src/ex_22082024/Lab040.py
@@ -1,10 +1,13 @@
 Marks = int(input("Enter your Marks"))
 match Marks:
-    case "90 to 100":
-        print("Distinction")
-    case "80 to 90":
+    case 90 | 100:
+        if Marks > 90 and Marks < 100:
+            print("Distinction")
+    case 80 | 90:
+       # elif Marks > 80 and Marks < 90:
         print("First Class")
-    case "70 to 80":
+    case 70 | 80:
+        # elif (Marks > 70 and Marks < 80):
         print("Second Class")
     case _:
         print("Third Class")
